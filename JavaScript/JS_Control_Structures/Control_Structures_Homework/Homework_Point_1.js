8b0189835026570c84709c0cac30a24c8b7e88c2
lucianN9 <lucian.nutu09@gmail.com>
2024-04-19 14:50:36 +0300

    Add one empty line between comment and code

--- JavaScript/JS_Control_Structures/Control_Structures_Homework/Homework_Point_1.js
+++ JavaScript/JS_Control_Structures/Control_Structures_Homework/Homework_Point_1.js
@@ -1,4 +1,5 @@
 // Display in the console the numbers from 1 to 20
+
 for (i = 1; i <= 20; i++) {
   console.log(i);
 }
--- JavaScript/JS_Control_Structures/Control_Structures_Homework/Homework_Point_4.js
+++ JavaScript/JS_Control_Structures/Control_Structures_Homework/Homework_Point_4.js
@@ -1,5 +1,4 @@
 // Compute the sum of the elements of an array and display it in the console
-
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = 0;
 
