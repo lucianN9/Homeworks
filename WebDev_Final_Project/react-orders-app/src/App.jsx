4ceff17fc5d6f0898422f0d1f84b66f970b8a98f
lucianN9 <lucian.nutu09@gmail.com>
2024-08-23 20:44:47 +0300

    Change import to TableButtons

--- WebDev_Final_Project/react-orders-app/src/App.jsx
+++ WebDev_Final_Project/react-orders-app/src/App.jsx
@@ -1,9 +1,11 @@
 import PageTitle from "./components/PageTitle";
+import TableButtons from "./components/TableButtons";
 
 const App = () => {
   return (
     <>
       <PageTitle />
+      <TableButtons />
     </>
   );
 };
