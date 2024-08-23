dc05b9db6f150f01b0e009f2665d70e0949f5b20
lucianN9 <lucian.nutu09@gmail.com>
2024-08-23 22:04:11 +0300

    Added imported button component

--- WebDev_Final_Project/react-orders-app/src/components/TableButtons.jsx
+++ WebDev_Final_Project/react-orders-app/src/components/TableButtons.jsx
@@ -1,31 +1,32 @@
 import "./TableButtons.css";
+import Button from "./Button";
 const TableButtons = () => {
   return (
     <>
       <div className="button-container">
         <div className="wrap-button">
-          <button type="button" className="btn btn-primary btn-lg button-style">
+          <Button>
             <p>
               TABLE <br />1
             </p>
-          </button>
-          <button type="button" className="btn btn-primary btn-lg button-style">
+          </Button>
+          <Button>
             <p>
               TABLE <br />2
             </p>
-          </button>
+          </Button>
         </div>
         <div className="wrap-button">
-          <button type="button" className="btn btn-primary btn-lg button-style">
+          <Button>
             <p>
               TABLE <br />3
             </p>
-          </button>
-          <button type="button" className="btn btn-primary btn-lg button-style">
+          </Button>
+          <Button>
             <p>
               TABLE <br />4
             </p>
-          </button>
+          </Button>
         </div>
       </div>
     </>
