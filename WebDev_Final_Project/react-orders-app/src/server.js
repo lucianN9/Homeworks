52e0030aec0191eb573e7e13427ef7c8d2374ac1
lucianN9 <lucian.nutu09@gmail.com>
2024-08-24 13:11:55 +0300

    Added GET functions for menu lists

--- WebDev_Final_Project/react-orders-app/src/server.js
+++ WebDev_Final_Project/react-orders-app/src/server.js
@@ -1,1 +1,32 @@
 // This is the main communication with the server
+
+const server = {
+  // Fetch function for the Main Course
+
+  async getMainCourse() {
+    const response = await fetch("http://localhost:3000/mainCourse");
+    const data = response.ok ? await response.json() : [];
+
+    return data;
+  },
+
+  // Fetch function for the Dessert
+
+  async getDessert() {
+    const response = await fetch("http://localhost:3000/dessert");
+    const data = response.ok ? await response.json() : [];
+
+    return data;
+  },
+
+  // Fetch function for the Drinks
+
+  async getDrinks() {
+    const response = await fetch("http://localhost:3000/drinks");
+    const data = response.ok ? await response.json() : [];
+
+    return data;
+  },
+};
+
+export default server;
